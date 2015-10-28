c9a277f7bf0da72ea13eeac653c0b99f2eb0f797
Frank <Backup@Franks-Mac-Pro.local>
2015-10-28 11:13:12 -0400

    Fixed requirements

--- coffeed/wsgi.py
+++ coffeed/wsgi.py
@@ -8,10 +8,9 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeed.settings")
-
 from django.core.wsgi import get_wsgi_application
-
 from dj_static import Cling
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeed.settings")
 
 application = Cling(get_wsgi_application())
